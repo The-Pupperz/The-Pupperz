580c4e0aca54e6890b3ccf3dd08b6dfbbd1b682d
Griffin <ffirgn@gmail.com>
2023-02-02 14:27:18 -0800

    home, login and profile logic added w styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import Nav from './components/Nav';
 import Landing from './components/Landing';
 import Login from './components/Login';
 import Register from './components/Register';
+import Support from './components/Support';
+import Home from './components/Home';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className=' w-full h-screen bg-[#040F16]'>
       <Nav />
-      <Landing />
+      <Home />
+      <Profile />
     </div>
 
   );
