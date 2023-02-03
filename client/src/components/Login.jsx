b1e5a0c3276e879f1beb437b983c7277d7e60a73
Jeremy <jdskinne@gmail.com>
2023-02-03 17:50:13 -0600

    added login and register functionality to client

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,34 +1,81 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 
-const Login = () => {
+import Auth from "../utils/auth.js";
+
+function Login() {
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [Login, { error, data }] = useMutation(LOGIN_USER);
+
+  const handleInputChange = ({ target: { name, value } }) => {
+    setFormState({ ...formState, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await Login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
-    <div class="w-[350px] mx-auto justify-center py-10">
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-            Username
+    <div className="w-[350px] mx-auto justify-center py-10">
+      <form onSubmit={handleFormSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
-
-          </input>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            name="email"
+            type="email"
+            value={formState.email}
+            onChange={handleInputChange}
+            placeholder="Email"
+          ></input>
         </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-        <input class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-
-        </input>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            name="password"
+            type="password"
+            value={formState.password}
+            onChange={handleInputChange}
+            placeholder="******************"
+          ></input>
+          <button
+            className="bg-[#A288E3] hover:bg-[#F79764] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+        {/* <div className="flex items-center justify-center">
+        </div> */}
+      </form>
     </div>
-    <div class="flex items-center justify-center">
-      <button class="bg-[#A288E3] hover:bg-[#F79764] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Sign In
-      </button>
-    </div>
-  </form>
-  
-</div>
-  )
+  );
 }
 
-export default Login
+export default Login;
