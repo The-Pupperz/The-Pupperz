c9959bd42e3c1a770cb81bc8f6b946ef85aaf270
Jeremy <jdskinne@gmail.com>
2023-02-04 16:42:15 -0600

    fixed create post and get posts on home

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,3 +25,84 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_POST = gql`
+  mutation createPost($postBody: String!, $name: String!) {
+    createPost(postBody: $postBody, name: $name) {
+      _id
+      postBody
+      createdAt
+      name
+    }
+  }
+`;
+
+export const ADD_REPLY = gql`
+  mutation addReply($replyBody: String!, $name: String!, $_id: ID!) {
+    addReply(replyBody: $replyBody, name: $name, _id: $_id) {
+      _id
+      replyBody
+      name
+    }   
+  }
+`;
+
+export const UPDATE_POST = gql`
+  mutation updatePost($postBody: String!, $_id: ID!, $name: String!) {
+    updatePost(postBody: $postBody, _id: $_id, name: $name) {
+      _id
+      postBody
+      createdAt
+      name
+    }
+  }
+`;
+
+export const UPDATE_REPLY = gql`
+  mutation updateReply($replyBody: String!, $_id: ID!, $name: String!) {
+    updateReply(replyBody: $replyBody, _id: $_id, name: $name) {
+      _id
+      replyBody
+      name
+    }
+  }
+`;
+
+export const REMOVE_POST = gql`
+  mutation removePost($_id: ID!) {
+    removePost(_id: $_id) {
+      _id
+      postBody
+      createdAt
+      name
+    }
+  }
+`;
+
+export const REMOVE_REPLY = gql`
+  mutation removeReply($_id: ID!) {
+    removeReply(_id: $_id) {
+      _id
+      replyBody
+      name
+    }
+  }
+`;
+
+export const ADD_FRIEND = gql`
+  mutation addFriend($_id: ID!) {
+    addFriend(_id: $_id) {
+      _id
+      name
+    }
+  }
+`;
+
+export const REMOVE_FRIEND = gql`
+  mutation removeFriend($_id: ID!) {
+    removeFriend(_id: $_id) {
+      _id
+      name
+    }
+  }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -9,3 +9,63 @@ export const QUERY_ME = gql`
     }
   }
 `;
+
+export const QUERY_POSTS = gql`
+  query getPosts {
+    getPosts {
+      _id
+      postBody
+      createdAt
+      name
+      replies {
+        _id
+        replyBody
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_POST = gql`
+  query getPost($_id: ID!) {
+    getPost(_id: $_id) {
+      _id
+      postBody
+      createdAt
+      name
+      replies {
+        _id
+        replyBody
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_USER_POSTS = gql`
+  query getUserPosts($_id: ID!) {
+    getUserPosts(_id: $_id) {
+      _id
+      postBody
+      createdAt
+      name
+      replies {
+        _id
+        replyBody
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_REPLIES = gql`
+  query getReplies($_id: ID!) {
+    getReplies(_id: $_id) {
+      _id
+      replyBody
+      name
+    }
+  }
+`;
+
+
