16e48133ed91c6c723ca65c0d8c6a77893002ac5
EMA1970 <114694158+EMA1970@users.noreply.github.com>
2023-02-05 13:01:09 -0800

    added font and made styling touches

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -50,9 +50,9 @@ function Home() {
       <div className="w-full bg-[#040F16] h-screen flex flex-row flex-wrap justify-center">
         <div className="w-full md:w-3/4 lg:w-3/5 p-5 md:px-12 lg:24 h-full overflow-x-scroll scrollbar-hide antialiased">
           <form onSubmit={handleFormSubmit}>
-            <div className="bg-[#040F16] w-full border-2 border-[#F79764] shadow rounded-lg p-5">
+            <div className="bg-[#a288e360] w-full shadow rounded-lg p-5">
               <textarea
-                className="bg-[#040F16] text-white w-full rounded-lg shadow border p-2"
+                className="bg-[#040F16] hover:translate-y-1 text-white w-full rounded-lg shadow font-Comfortaa p-2 hover:"
                 name="postBody"
                 rows="5"
                 value={formState.postBody}
@@ -62,10 +62,10 @@ function Home() {
 
               <div className="w-full flex flex-row flex-wrap mt-3">
                
-                <div className="w-2/3">
+                <div className="w-2/3 flex">
                   <button
                     type="Submit"
-                    className="float-right bg-[#A288E3] hover:bg-[#F79764] text-white p-2 rounded-lg font-Comfortaa"
+                    className=" float-right hover:translate-y-1 bg-[#A288E3] hover:bg-[#F79764] text-white p-2 rounded-lg font-Comfortaa"
                   >
                     Submit
                   </button>
@@ -77,25 +77,25 @@ function Home() {
           {posts.map((post) => (
             <div className="mt-3 flex flex-col">
               <div className="bg-[#040F16] mt-3">
-                <div className="bg-[#95553375] font- shadow p-3 text-3xl text-white font-semibold">
+                <div className=" heading bg-[#a288e383] rounded-md shadow p-3 text-3xl text-white font-semibold">
                   <div style={{ cursor: "pointer" }} onClick={handleClick}>
                     {post.name}
                   </div>
                   {showProfile && <Profile />}
                 </div>
-                <div className="bg-[#040F16] border border-[#A288E3] shadow p-5 text-xl text-white font-semibold">
+                <div className="bg-[#040F16] rounded-md border-2 border-[#A288E3] shadow p-5 text-xl text-white font-Comfortaa">
                   {post.postBody}
                 </div>
                 <div className="bg-[#a288e393] p-1 border  shadow flex flex-row flex-wrap justify-around">
-                  <div className="w-1/3 hover:bg-[#F79764] border-2  border-[#A288E3] text-center my-1 py2-3 text-l text-white font-Comfortaa rounded-md">
+                  <div className="w-1/3 hover:bg-[#F79764] border-2  border-[#A288E3] text-center my-1 py2-3 text-l  text-white font-Comfortaa rounded-md bg-[#A288E3] hover:translate-y-1 cursor-pointer">
                     Like
                   </div>
-                  <div className="w-1/3 hover:bg-[#F79764] border-2 border-[#A288E3] text-center my-1 py2-3 text-l text-white font-Comfortaa rounded-md">
+                  <div className="w-1/3 hover:bg-[#F79764] border-2 border-[#A288E3] text-center my-1 py2-3 text-l text-white font-Comfortaa rounded-md bg-[#A288E3] hover:translate-y-1 cursor-pointer">
                     Comment
                   </div>
                 </div>
 
-                <div className="bg-[#040F16] border bg-[#040F16] border-[#A288E3] rounded-b-lg shadow p-5 text-l text-white content-center font-semibold flex flex-row flex-wrap">
+                <div className=" heading bg-[#040F16] border-[#A288E3] rounded-b-lg shadow p-5 text-l text-white content-center font-semibold flex flex-row flex-wrap">
                   <div className="w-full">
                     <div className="w-full text-left text-2xl underline text-[#F79764] py-1">
                       {/* {post.replies.length} Comments */}
