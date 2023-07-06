1977ad782363d88b786838cee90171a6777dc7c9
Jeremy <jdskinne@gmail.com>
2023-07-05 20:31:30 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -28,6 +28,10 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "client", "build", "index.html"));
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "client", "build", "index.html"));
+});
+
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
