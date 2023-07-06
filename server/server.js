719c7ce17825d7c9a44f0e04682880cb786ad5d4
Jeremy <jdskinne@gmail.com>
2023-07-05 21:37:15 -0500

    redeploying

--- server/server.js
+++ server/server.js
@@ -28,9 +28,7 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "client", "build", "index.html"));
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, "client", "build", "index.html"));
-});
+
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
