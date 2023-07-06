b322a6b8194882ab603d9de552ea622dd5ad1026
Jeremy <jdskinne@gmail.com>
2023-07-05 22:10:40 -0500

    rere

--- server/server.js
+++ server/server.js
@@ -24,9 +24,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(join(__dirname, "..", "client", "build")));
 }
 
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "client", "build", "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(join(__dirname, "client", "build", "index.html"));
+// });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
